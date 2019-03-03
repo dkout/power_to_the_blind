78f393a08eef7aa67066f780a28efa5d1d2ce63a
Dimitris <dkout@mit.edu>
2019-03-02 20:34:44 -0500

    change scaling

--- face_recognition_video.py
+++ face_recognition_video.py
@@ -62,7 +62,7 @@ while True:
     ret, frame = video_capture.read()
 
     # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    small_frame = cv2.resize(frame, (0, 0), fx=0.1, fy=0.1)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
